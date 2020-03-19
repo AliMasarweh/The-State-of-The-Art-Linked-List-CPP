9005263e42ba032eb701954fc2b6a687ccb61e7c
Ali Masarwe <alis.masarwe@gmail.com>
2020-03-19 05:18:52 -0700

    The basic functionality of the unrolled linked list, contuining to thread safety

--- linked_list.h
+++ linked_list.h
@@ -31,7 +31,7 @@ namespace soa {
 
         size_t capacity() const;
 
-        const T &operator[](size_t) const;
+        T &operator[](size_t);
 
         T &at(size_t);
 
@@ -40,14 +40,32 @@ namespace soa {
         size_t m_size;
         size_t m_capacity;
         Node<T, SIZE>* m_next;
+        Node<T, SIZE>* m_prev;
     };
 
     template<class T, unsigned int SIZE>
     class LinkedList {
 
-        friend bool operator<(const LinkedList<T, SIZE> &l1, const LinkedList<T, SIZE> &l2);
-        friend bool operator==(const LinkedList<T, SIZE> &l1, const LinkedList<T, SIZE> &l2);
-        friend bool operator!=(const LinkedList<T, SIZE> &l1, const LinkedList<T, SIZE> &l2);
+        friend bool operator==(const LinkedList<T, SIZE> &l1, const LinkedList<T, SIZE> &l2)
+        {
+            Iterator iterator1(l1);
+            Iterator iterator2(l2);
+
+            while(iterator1.hasNext() && iterator2.hasNext())
+            {
+                if(*iterator1 != *iterator2)
+                    return false;
+                ++iterator1;
+                ++iterator2;
+            }
+
+            return !(iterator1.hasNext() || iterator2.hasNext());
+        }
+
+        friend bool operator!=(const LinkedList<T, SIZE> &l1, const LinkedList<T, SIZE> &l2)
+        {
+            return !(l1 == l2);
+        }
 
     public:
 
@@ -68,15 +86,17 @@ namespace soa {
             friend class LinkedList<T, SIZE>;
         public:
             explicit Iterator(LinkedList<T, SIZE>* lstToIterate);
-            Iterator& operator++(int);
+            Iterator operator++(int);
             Iterator& operator++();
-            Node<T, SIZE>& operator->();
+            Node<T, SIZE>* operator->();
             Node<T, SIZE>& operator*();
-            bool haseNext();
+            Iterator& next();
+            bool hasNext();
 
             const static Iterator s_end = NULL;
         private:
             Node<T, SIZE>* m_pntrToTraverse;
+            size_t m_indx;
         };
 
         using iterator = LinkedList<T, SIZE>::Iterator;
@@ -106,8 +126,6 @@ namespace soa {
         void push_front(const T &value);
 
         void pop_front();
-
-        void resize(size_t count, T value = T());
 
     private:
         Node<T, SIZE> m_head;
@@ -249,50 +267,177 @@ namespace soa {
     {
         iterator tmp = iterator(pos);
         this->shiftToRight(tmp);
-        pos->m_data = value;
+        pos.m_pntrToTraverse->m_data = value;
         return ++pos;
     }
 
     template<class T, unsigned int SIZE>
-    LinkedList::iterator LinkedList<T, SIZE>::erase(LinkedList::iterator& pos)
+    typename LinkedList<T, SIZE>::iterator& LinkedList<T, SIZE>::erase(LinkedList::iterator& pos)
     {
-        for (int i = pos.operator->().size() - 1; i < ; ++i)
+        for (int i = pos.m_indx; i < pos->size() - 2; ++i)
         {
-
+            pos->at(i) = pos->at(i+1);
         }
-        pos.operator->().at()
-        return soa::LinkedList::iterator();
+
+        return pos;
     }
 
     template<class T, unsigned int SIZE>
     void LinkedList<T, SIZE>::push_back(const T &value)
     {
-
+        if(m_tail->m_size == m_tail->m_capacity)
+        {
+            m_tail->m_next = new Node<T, SIZE>(m_tail->m_capacity);
+            m_tail->m_next->m_prev = m_tail;
+            m_tail = m_tail->m_next;
+        }
+        m_tail->at(m_tail->m_size ++) = value;
     }
 
     template<class T, unsigned int SIZE>
     void LinkedList<T, SIZE>::pop_back()
     {
-
+        if(m_tail->m_size == 0)
+        {
+            m_tail = m_tail->m_prev;
+            delete m_tail->m_next;
+        } else
+            -- m_tail->m_size;
     }
 
     template<class T, unsigned int SIZE>
     void LinkedList<T, SIZE>::push_front(const T &value)
     {
-
+        Node<T, SIZE>* headCopy = new Node<T, SIZE>(m_head);
+        m_head.m_size = 0;
+        m_head.m_next = headCopy;
+        m_head.at(m_size++) = value;
     }
 
     template<class T, unsigned int SIZE>
     void LinkedList<T, SIZE>::pop_front()
     {
-
+        Node<T, SIZE> afterHeadCopy(m_head.m_next);
+        if(m_head->m_size == 0)
+        {
+            delete m_head.m_next;
+            m_head = m_head.m_next;
+            m_head = afterHeadCopy;
+        } else
+            -- m_head->m_size;
     }
 
     template<class T, unsigned int SIZE>
-    void LinkedList<T, SIZE>::resize(size_t count, T value)
-    {
+    LinkedList<T, SIZE>::Iterator::Iterator(LinkedList<T, SIZE> *lstToIterate): m_indx(0),
+            m_pntrToTraverse(& lstToIterate->m_head){}
 
+template<class T, unsigned int SIZE>
+    typename LinkedList<T, SIZE>::Iterator LinkedList<T, SIZE>::Iterator::operator++(int)
+    {
+        if(m_pntrToTraverse->m_size == m_pntrToTraverse->m_capacity)
+            m_pntrToTraverse = m_pntrToTraverse->m_next;
+        LinkedList<T, SIZE>::Iterator ans(*this);
+        ++m_pntrToTraverse->m_size;
+
+        return ans;
     }
 
+    template<class T, unsigned int SIZE>
+    typename LinkedList<T, SIZE>::Iterator &LinkedList<T, SIZE>::Iterator::operator++()
+    {
+        if(m_pntrToTraverse->m_size == m_pntrToTraverse->m_capacity)
+            m_pntrToTraverse = m_pntrToTraverse->m_next;
+        ++m_pntrToTraverse->m_size;
+
+        return *this;
+    }
+
+    template<class T, unsigned int SIZE>
+    Node<T, SIZE>* LinkedList<T, SIZE>::Iterator::operator->() {
+        return m_pntrToTraverse;
+    }
+
+    template<class T, unsigned int SIZE>
+    Node<T, SIZE> &LinkedList<T, SIZE>::Iterator::operator*() {
+        return *m_pntrToTraverse;
+    }
+
+    template<class T, unsigned int SIZE>
+    typename LinkedList<T, SIZE>::Iterator &LinkedList<T, SIZE>::Iterator::next() {
+        ++this;
+        return *this;
+    }
+
+    template<class T, unsigned int SIZE>
+    bool LinkedList<T, SIZE>::Iterator::hasNext()
+    {
+        return m_pntrToTraverse->m_size < m_pntrToTraverse->m_capacity ||
+               m_pntrToTraverse->m_next != NULL;
+    }
+
+    template<class T, unsigned int SIZE>
+    Node<T, SIZE>::Node(size_t capacity):m_capacity(m_capacity), m_size(0), m_next(NULL), m_prev(NULL){}
+
+    template<class T, unsigned int SIZE>
+    Node<T, SIZE>::Node(Node<T, SIZE> &toCopy):m_capacity(m_capacity), m_size(0)
+    {
+        while(this->m_size < toCopy.m_size)
+        {
+            this[this->m_size++] = toCopy[this->m_size];
+        }
+
+        m_next = toCopy.m_next;
+        m_prev = toCopy.m_prev;
+    }
+
+    template<class T, unsigned int SIZE>
+    Node<T, SIZE> &Node<T, SIZE>::operator=(Node<T, SIZE> &toCopy)
+    {
+        if(this != &toCopy)
+        {
+            this->m_capacity = toCopy.m_capacity;
+            this->m_size = 0;
+            while(this->m_size < toCopy.m_size)
+            {
+                this[this->m_size++] = toCopy[this->m_size];
+            }
+        }
+
+        return *this;
+    }
+
+    template<class T, unsigned int SIZE>
+    Node<T, SIZE>::~Node<T, SIZE>() {}
+
+    template<class T, unsigned int SIZE>
+    bool Node<T, SIZE>::empty() const {
+        return m_size;
+    }
+
+    template<class T, unsigned int SIZE>
+    size_t Node<T, SIZE>::size() const {
+        return m_size;
+    }
+
+    template<class T, unsigned int SIZE>
+    size_t Node<T, SIZE>::capacity() const {
+        return m_capacity;
+    }
+
+    template<class T, unsigned int SIZE>
+    T &Node<T, SIZE>::operator[](size_t index) {
+        if(index < m_capacity)
+            return this->m_data[index];
+
+        throw -1;// data not found
+    }
+
+    template<class T, unsigned int SIZE>
+    T &Node<T, SIZE>::at(size_t index) {
+        if(index < m_capacity)
+            return this->m_data[index];
+
+        throw -1;
+    }
 
 #endif //THE_STATE_OF_THE_ART_LINKED_LIST_LINKED_LIST_H
